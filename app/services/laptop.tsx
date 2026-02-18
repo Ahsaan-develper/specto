ff5a8e5f0bcc641d2d4ff0b449d064aa0f76780e
Ahsaan Ali <aliahsan80152@gamil.com>
2026-02-18 19:25:56 +0500

    service

--- app/services/laptop.tsx
+++ app/services/laptop.tsx
@@ -1,9 +1,13 @@
 export default function Laptop(){
     return (
         <section className="flex justify-center  relative items-center">
-          <img className="absolute rotate-30 rotate-x-180   top-4 left-60 w-full max-w-40" src="laptopScreen.svg" alt="" />
-          <img className="absolute rotate-30 top-20 -rotate-x-180 left-40 w-full max-w-40" src="laptopScreenBack.svg" alt="" />
-          <img className="absolute -rotate-30 top-36 left-44 w-full max-w-26" src="kayboardBlue5dfb.webp" alt="" />
+            <img className="absolute rotate-x-50 rotate-46 -rotate-y-30 top-22 left-34 w-full max-w-54" src="laptopScreenUppeback.svg" alt="" />
+            <img className="absolute rotate-x-50 rotate-46 -rotate-y-30 top-22 left-34 w-full max-w-54" src="laptopBase.webp" alt="" />
+            <img className="absolute rotate-x-50 rotate-46 -rotate-y-30 top-32 left-34 w-full max-w-50 " src="laptopBaseOutline.svg" alt="" />
+          <img className="absolute  rotate-y-54  top-3 left-57 w-full max-w-40" src="laptopScreenUppeback.svg" alt="" />
+          <img className="absolute  rotate-y-54  top-4.5 left-58 w-full max-w-38" src="laptopScreen.svg" alt="" />
+          {/* <img className="absolute rotate-x-50 rotate-45 -rotate-y-30 top-20 left-46 w-full max-w-40" src="laptopScreenBack.svg" alt="" /> */}
+          <img className="absolute rotate-20 top-30 left-50 w-full max-w-22" src="kayboardBlue5dfb.webp" alt="" />
         </section>
     )
 }
